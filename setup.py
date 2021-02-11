6139dbad50b6ca417df374b185aefb9879d22004
VitoPaoloPastore <51142446+VitoPaoloPastore@users.noreply.github.com>
2021-02-11 17:20:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     author_email="vitopaolopastore@gmail.com",
     description="Toolbox for image processing- and deep-learning - based cell filopodia detection and analysis",
     long_description="Toolbox for image processing- and deep-learning - based cell filopodia detection and analysis",
-    url="https://github.com/VitoPaoloPastore/Filo_Analyzer",
+    url="https://github.com/VitoPaoloPastore/Filo_Analyzer.git",
     packages=setuptools.find_packages(),
     install_requires = ['absl-py==0.10','Keras-Applications==1.0.8',
 'Keras-Preprocessing==1.1.2','opencv-python==4.4.0.46','pandas==1.0.4',
